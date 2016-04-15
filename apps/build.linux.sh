cd7b282cfe8ca06f09c08dabc411be9be0f78dfd
Guillaume Chereau <guillaume@noctua-software.com>
2016-04-15 19:53:17 +0800

    Fix linux build script
    Tested on debian only.

--- apps/build.linux.sh
+++ apps/build.linux.sh
@@ -2,8 +2,14 @@ mkdir -p bin
 
 cc="clang"
 cflags="-Ofast -ffast-math -funroll-loops -std=gnu99"
-linkflags="-lm -lpthread"
+linkflags="-lm -lpthread -lGL -lGLEW `pkg-config --libs glfw3`"
 
-$cc -DYA_NOGL $cflags $linkflags apps/ytrace.c -o bin/ytrace-cli
-$cc $cflags $linkflags apps/ytestgen.c -o bin/ytestgen
+cp apps/render_tests.sh bin/
+
+$cc $cflags $linkflags apps/yview.c -o bin/yview &
+$cc $cflags $linkflags apps/ytrace.c -o bin/ytrace &
+$cc -DYA_NOGL $cflags $linkflags apps/ytrace.c -o bin/ytrace-cli &
+$cc $cflags $linkflags apps/yshade.c -o bin/yshade &
+$cc $cflags $linkflags apps/ytestgen.c -o bin/ytestgen &
+wait
 
